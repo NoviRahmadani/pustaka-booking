0ba87a643d097123144e78e901177ef5c1ac66de
NoviRahmadani <novirahmadani900@gmail.com>
2021-10-10 18:37:42 +0700

    Latihan pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,20 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan: Novi</li>
+        <li>Nama Belakang: Rahmadani</>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Griya Taman Walet Blok GW 10 NO 26</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Basket</li>
+    </ul>
+</section>
+    
