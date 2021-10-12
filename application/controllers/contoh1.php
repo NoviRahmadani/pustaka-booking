cca79523482a85e1e9dbc3a77b3c949c88945091
NoviRahmadani <novirahmadani900@gmail.com>
2021-10-12 17:41:50 +0700

    menambahkan application/controllers/contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,16 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+}
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya Novi Rahmadani
+         saya tinggal di daerah Tangerang
+         olahraga yang saya sukai adalah 
+         Bulutangkis;
+
+}
+ 
+
+}
+
